32d636720672a9b5a5f6c92c892197e70231bc95
Leon Eck <leoneck@Leons-MBP.fritz.box>
2015-11-14 08:23:22 +0100

    When there is more than one player on the site, a game is created with two players

--- app.js
+++ app.js
@@ -2,10 +2,47 @@ var express = require("express"),
 		app = express(),
 		server = require("http").createServer(app),
 		io = require("socket.io").listen(server)
-		server.listen(8000);
+		server.listen(8000),
+		lobbyBuffer = [],
+		runningGames = new Map();
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
 app.use(express.static(__dirname + '/'));
+
+io.sockets.on("connection", function (socket) {
+
+	playerConnected(socket.id);
+
+	socket.on("disconnect", function () {
+		// TODO: Socketid aus dem LobbyBuffer entfernen
+		// TODO: Wenn der Spieler noch in einem Spiel ist, soll dieses geschlossen werden
+	});
+
+});
+
+function playerConnected (socketId) {
+	if (lobbyBuffer.length === 0) {
+		lobbyBuffer.push(socketId);
+	} else {
+		createNewGame(socketId, lobbyBuffer.pop());
+	}
+}
+
+function createNewGame (playerOneId, playerTwoId) {
+	runningGames.set(runningGames.size, 
+		{ "players": {
+				"playerOne": playerOneId,
+				"playerTwo": playerTwoId
+			}
+		}
+	);
+	informPlayerAboutGameStart(playerOneId, playerTwoId);
+	informPlayerAboutGameStart(playerTwoId, playerOneId);
+}
+
+function informPlayerAboutGameStart (playerId, opponentId) {
+	io.sockets.to(playerId).emit("gameIsStarting", true);
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
+	var socket = io.connect();
 
+	socket.on("gameIsStarting", function (data) {
+		$("#lobbyContainer").hide();
+		$("#gameContainer").show();
+	});
+
+});
