e6fbb21c344a352058d8f6d9bfd46c6de0461f47
LeonEck <eck.leon95@gmail.com>
2015-11-15 14:59:55 +0100

    Switched game field to table

--- js/main.js
+++ js/main.js
@@ -16,14 +16,19 @@ $(document).ready(function () {
 });
 
 function generateGameFields () {
-	var gameFieldHtml = "";
-	for(var i = 0; i < 10; i++) {
-		gameFieldHtml += "<div class='gameFieldRow'>";
-		for(var j = 0; j < 10; j++) {
-			gameFieldHtml += "<div class='gameFieldSquare'></div>";
-		}
-		gameFieldHtml += "</div>";
+	var characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+	var gameFieldHtml = "<table><tr><td class='gameFieldHeaderSquare'></td>";
+	for(var i = 1; i <= 10; i++) {
+		gameFieldHtml += "<td class='gameFieldHeaderSquare'>" + i + "</td>";
 	}
-
+	gameFieldHtml += "</tr>"
+	for(var i = 0; i < 10; i++) {
+		gameFieldHtml += "<tr><td class='gameFieldHeaderSquare'>" + characters[i] + "</td>";
+		for(var j = 0; j < 10; j++) {
+			gameFieldHtml += "<td class='gameFieldSquare'></td>";
+		}
+		gameFieldHtml += "</tr>";
+	}
+	gameFieldHtml += "</table>"
 	$(".gameField").append(gameFieldHtml);
 }
