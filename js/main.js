1039332ab1c2cc6b69448d5c6f5883046f9d9de1
LeonEck <eck.leon95@googlemail.com>
2015-12-28 16:44:49 +0100

    Refactored client

--- js/main.js
+++ js/main.js
@@ -1,112 +1,123 @@
-"use strict";
+'use strict';
 $(document).ready(function () {
-	var socket = io.connect();
+  var socket = io.connect();
 
-	generateGameFields();
+  $(document).on('click', '#searchForGameButton', function () {
+    $('#searchForGameButton').text('Searching for game...');
+    socket.emit('searchingForGame', true);
+  });
 
-	socket.on("gameIsStarting", function (data) {
-		$("#lobbyContainer").hide();
-		$("#gameContainer").show();
-	});
+  $(document).on('click', '#opponentGameField .gameFieldSquare', function () {
+    socket.emit('clickOnOpponentGameField', $(this).attr('squareNumber'));
+  });
 
-	socket.on("gameIsAborted", function (data) {
-		alert("Game sborted. Please reload the page to search for a new game");
-		$("#gameContainer").hide();
-	});
+  socket.on('gameIsStarting', function () {
+    $('#lobbyContainer').hide();
+    generateGameFields();
+    $('#gameContainer').show();
+  });
 
-	socket.on("gameField", function (data) {
-		$("myGameField .shipPart").removeClass("shipPart");
-		for(var i = 0; i < data.length; i++) {
-			switch (data[i].substring(0,1)) {
-				case "x":
-					$("#myGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("shipPart");
-					break;
+  socket.on('gameIsAborted', function () {
+    returnToLobbyWithAlert('gameWasClosed');
+  });
 
-				case "d":
-					$("#myGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("shipPartHit");
-					break;
+  socket.on('gameField', function (data) {
+    fillGameField('myGameField', data);
+  });
 
-				case "z":
-					$("#myGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("missed");
-					break;
+  socket.on('opponentGameField', function (data) {
+    fillGameField('opponentGameField', data);
+  });
 
-				case "k":
-					$("#myGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("fullyDestroyedShip");
-					break;
+  socket.on('isItMyTurn', function (data) {
+    if (data) {
+      $('#myTitle').addClass('text-danger');
+      $('#opponentTitle').removeClass('text-danger');
+    } else {
+      $('#myTitle').removeClass('text-danger');
+      $('#opponentTitle').addClass('text-danger');
+    }
+  });
 
-				default:
-					break;
-			}
-		}
-	});
+  socket.on('won', function (data) {
+    if (data) {
+      returnToLobbyWithAlert('gameWon');
+    } else {
+      returnToLobbyWithAlert('gameLost');
+    }
+  });
 
-	socket.on("opponentGameField", function (data) {
-		$("#opponentGameField .shipPart").removeClass("shipPart");
-		for(var i = 0; i < data.length; i++) {
-			switch (data[i].substring(0,1)) {
-				case "x":
-					$("#opponentGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("shipPart");
-					break;
 
-				case "d":
-					$("#opponentGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("shipPartHit");
-					break;
-
-				case "z":
-					$("#opponentGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("missed");
-					break;
-
-				case "k":
-					$("#opponentGameField .gameFieldSquare[squareNumber=" + i + "]").addClass("fullyDestroyedShip");
-					break;
-
-				default:
-					break;
-			}
-		}
-	});
-
-	socket.on("isItMyTurn", function (data) {
-		console.log("here");
-		if(data) {
-			$("#myTitle").addClass("textRed");
-			$("#opponentTitle").removeClass("textRed");
-		} else {
-			$("#myTitle").removeClass("textRed");
-			$("#opponentTitle").addClass("textRed");
-		}
-	});
-
-	socket.on("won", function (data) {
-		if(data) {
-			alert("You won!");
-		} else {
-			alert("You lost!");
-		}
-	});
-
-	$(document).on("click", "#opponentGameField .gameFieldSquare", function () {
-		socket.emit("clickOnOpponentGameField", $(this).attr("squareNumber"));
-	});
 
 });
 
 function generateGameFields () {
-	var characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-	var gameFieldHtml = "<table><tr><td class='gameFieldHeaderSquare'></td>";
-	for(var i = 1; i <= 10; i++) {
-		gameFieldHtml += "<td class='gameFieldHeaderSquare'>" + i + "</td>";
-	}
-	gameFieldHtml += "</tr>";
-	var squareCount = 0;
-	for(var i = 0; i < 10; i++) {
-		gameFieldHtml += "<tr><td class='gameFieldHeaderSquare'>" + characters[i] + "</td>";
-		for(var j = 0; j < 10; j++) {
-			gameFieldHtml += "<td class='gameFieldSquare' squareNumber='" + squareCount + "'></td>";
-			squareCount++;
-		}
-		gameFieldHtml += "</tr>";
-	}
-	gameFieldHtml += "</table>"
-	$(".gameField").html(gameFieldHtml);
+  $('.gameField').empty();
+  var characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  var gameFieldHtml = '<table class="margin-auto"><tr><td class="gameFieldHeaderSquare"></td>';
+  for(var i = 1; i <= 10; i++) {
+    gameFieldHtml += '<td class="gameFieldHeaderSquare">' + i + '</td>';
+  }
+  gameFieldHtml += '</tr>';
+  var squareCount = 0;
+  for(var j = 0; j < 10; j++) {
+    gameFieldHtml += '<tr><td class="gameFieldHeaderSquare">' + characters[j] + '</td>';
+    for(var k = 0; k < 10; k++) {
+      gameFieldHtml += '<td class="gameFieldSquare" squareNumber="' + squareCount + '"></td>';
+      squareCount++;
+    }
+    gameFieldHtml += '</tr>';
+  }
+  gameFieldHtml += '</table>';
+  $('.gameField').html(gameFieldHtml);
+}
+
+function fillGameField (targetGameField, data) {
+  $('#' + targetGameField + ' .shipPart').removeClass('shipPart');
+  for(var i = 0; i < data.length; i++) {
+    switch (data[i].substring(0,1)) {
+      case 'x':
+        $('#' + targetGameField + ' .gameFieldSquare[squareNumber=' + i + ']').addClass('shipPart');
+        break;
+
+      case 'd':
+        $('#' + targetGameField + ' .gameFieldSquare[squareNumber=' + i + ']').addClass('shipPartHit');
+        break;
+
+      case 'z':
+        $('#' + targetGameField + ' .gameFieldSquare[squareNumber=' + i + ']').addClass('missed');
+        break;
+
+      case 'k':
+        $('#' + targetGameField + ' .gameFieldSquare[squareNumber=' + i + ']').addClass('fullyDestroyedShip');
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+function showAlert (alertToShow) {
+  $('#gameWasClosedAlert').hide();
+  $('#gameWonAlert').hide();
+  $('#gameLostAlert').hide();
+  switch (alertToShow) {
+    case 'gameWasClosed':
+      $('#gameWasClosedAlert').show();
+      break;
+    case 'gameWon':
+      $('#gameWonAlert').show();
+      break;
+    case 'gameLost':
+      $('#gameLostAlert').show();
+      break;
+  }
+}
+
+function returnToLobbyWithAlert (alertToShow) {
+  $('#gameContainer').hide();
+  showAlert(alertToShow);
+  $('#searchForGameButton').text('Search for game');
+  $('#lobbyContainer').show();
 }
