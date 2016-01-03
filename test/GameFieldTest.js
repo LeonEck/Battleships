429e2347bc13493ad0d80d978b4907efeb0f0ac9
LeonEck <eck.leon95@googlemail.com>
2016-01-03 20:25:11 +0100

    Started with game field tests

---
+++ test/GameFieldTest.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let assert = require('assert');
+let GameField = require("../includes/GameField");
+
+describe('GameField Test', () => {
+
+  let gameField;
+
+  /**
+   * A valid game field represented as a flat array for testing
+   * @type {Array}
+   */
+  const testFlatArrayGameField = ['x9', 'x9', 'x9', 'o', 'x5', 'o', 'o', 'x3', 'o', 'o', 'o', 'o', 'o', 'o', 'x5', 'o', 'o', 'x3', 'o', 'x4', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x4', 'o', 'o', 'x7', 'o', 'o', 'o', 'o', 'o', 'o', 'x4', 'x6', 'o', 'x7', 'o', 'o', 'o', 'o', 'o', 'o', 'x4', 'x6', 'o', 'x7', 'o', 'o', 'x0', 'o', 'o', 'o', 'x4', 'x6', 'o', 'x7', 'o', 'o', 'x0', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x2', 'x1', 'x1', 'x1', 'x1', 'o', 'x8', 'x8', 'x8', 'o', 'x2'];
+
+  beforeEach(() => {
+    gameField = new GameField(10);
+  });
+
+  describe('isLocked', () => {
+    it('should return true when the game field is locked', () => {
+      gameField.lock();
+      assert.strictEqual(gameField.isLocked(), true, 'Game field is locked');
+    });
+
+    it('should return false when the game field was not locked', () => {
+      assert.strictEqual(gameField.isLocked(), false, 'Game field is not locked');
+    });
+  });
+
+  describe('lock', () => {
+    it('should lock the game field', () => {
+      gameField.lock();
+      assert.strictEqual(gameField.isLocked(), true, 'Game field is locked');
+    });
+  });
+
+  describe('Game field generation', () => {
+    it('should generate a valid game field', () => {
+      gameField.generateGameField();
+      assert.strictEqual(gameField.isValidGameField(gameField.makeFlatArray()), true, 'Generated game field is valid');
+    });
+  });
+
+  describe('isValidGameField', () => {
+    it('should return true for a valid game field', () => {
+      assert.strictEqual(gameField.isValidGameField(testFlatArrayGameField), true, 'Given game field is valid');
+    });
+  });
+
+  describe('loadFlatArray & makeFlatArray', () => {
+    it('should successfully load in a flat array and return the same when requested with makeFlatArray', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      const returnValue = gameField.makeFlatArray();
+      assert.deepEqual(testFlatArrayGameField, returnValue, 'Input and output arrays should be the same');
+    });
+  });
+
+});
