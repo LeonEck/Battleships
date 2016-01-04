21eeba9761a7172ca67b4821fbaa11658ddbb106
LeonEck <eck.leon95@googlemail.com>
2016-01-04 15:33:47 +0100

    Added more game field tests

--- test/GameFieldTest.js
+++ test/GameFieldTest.js
@@ -28,13 +28,6 @@ describe('GameField Test', () => {
     });
   });
 
-  describe('lock', () => {
-    it('should lock the game field', () => {
-      gameField.lock();
-      assert.strictEqual(gameField.isLocked(), true, 'Game field is locked');
-    });
-  });
-
   describe('Game field generation', () => {
     it('should generate a valid game field', () => {
       gameField.generateGameField();
@@ -45,6 +38,12 @@ describe('GameField Test', () => {
   describe('isValidGameField', () => {
     it('should return true for a valid game field', () => {
       assert.strictEqual(gameField.isValidGameField(testFlatArrayGameField), true, 'Given game field is valid');
+    });
+
+    it('should return false for a not valid game field', () => {
+      let testInvalidGameField = testFlatArrayGameField.splice();
+      testInvalidGameField[0] = 'asd';
+      assert.strictEqual(gameField.isValidGameField(testInvalidGameField), false, 'Game field invalid');
     });
   });
 
@@ -109,6 +108,95 @@ describe('GameField Test', () => {
       gameField.loadFlatArray(testFlatArrayGameField);
       gameField.setMissed(3);
       assert.deepEqual(gameField.isIntactShip(3), false, 'Fourth field is a missed field');
+    });
+  });
+
+  describe('clickOnShipPart', () => {
+    it('should set an intact ship part to detstroyed', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      gameField.clickOnShipPart(0);
+      assert.deepEqual(gameField.isIntactShip(0), false, 'The first ship part is destroyed');
+    });
+  });
+
+  describe('setMissed', () => {
+    it('should set water to a missed field', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      gameField.setMissed(3);
+      assert.deepEqual(gameField.isClickableField(3), false, 'The fourth field was set to missed field');
+    });
+  });
+
+  describe('areNotFullyDestroyedShipPartsLeft', () => {
+    it('should detect that there are still not fully destroyed ship parts left', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      assert.deepEqual(gameField.areNotFullyDestroyedShipPartsLeft(), true, 'There are still not fully destroyed ship parts left');
+    });
+
+    it('should detect that all ships have been fully destroyed', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      gameField.clickOnShipPart(0);
+      gameField.clickOnShipPart(1);
+      gameField.clickOnShipPart(2);
+      gameField.clickOnShipPart(4);
+      gameField.clickOnShipPart(7);
+      gameField.clickOnShipPart(14);
+      gameField.clickOnShipPart(17);
+      gameField.clickOnShipPart(19);
+      gameField.clickOnShipPart(29);
+      gameField.clickOnShipPart(32);
+      gameField.clickOnShipPart(39);
+      gameField.clickOnShipPart(40);
+      gameField.clickOnShipPart(42);
+      gameField.clickOnShipPart(49);
+      gameField.clickOnShipPart(50);
+      gameField.clickOnShipPart(52);
+      gameField.clickOnShipPart(55);
+      gameField.clickOnShipPart(59);
+      gameField.clickOnShipPart(60);
+      gameField.clickOnShipPart(62);
+      gameField.clickOnShipPart(65);
+      gameField.clickOnShipPart(89);
+      gameField.clickOnShipPart(90);
+      gameField.clickOnShipPart(91);
+      gameField.clickOnShipPart(92);
+      gameField.clickOnShipPart(93);
+      gameField.clickOnShipPart(95);
+      gameField.clickOnShipPart(96);
+      gameField.clickOnShipPart(97);
+      gameField.clickOnShipPart(99);
+      assert.deepEqual(gameField.areNotFullyDestroyedShipPartsLeft(), false, 'All ships are destroyed');
+    });
+  });
+
+  describe('makeFlatArray', () => {
+    it('should generate a flat array', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      assert.deepEqual(gameField.makeFlatArray(), testFlatArrayGameField, 'Correctly created a flat array');
+    });
+  });
+
+  describe('makeAnonymousFlatArray', () => {
+    it('should generate an anonymous flat array with only water when created', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      const expectedValue = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
+      assert.deepEqual(gameField.makeAnonymousFlatArray(), expectedValue, 'Correctly created an anonymous flat array consisting of only water');
+    });
+
+    it('should generate an anonymous flat array where destroyed ship parts are displayed', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      gameField.clickOnShipPart(0);
+      const expectedValue = ['d', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
+      assert.deepEqual(gameField.makeAnonymousFlatArray(), expectedValue, 'Correctly created an anonymous flat array showing destroyed ship parts');
+    });
+
+    it('should generate an anonymous flat array where fully destroyed ship parts are displayed', () => {
+      gameField.loadFlatArray(testFlatArrayGameField);
+      gameField.clickOnShipPart(0);
+      gameField.clickOnShipPart(1);
+      gameField.clickOnShipPart(2);
+      const expectedValue = ['k', 'k', 'k', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
+      assert.deepEqual(gameField.makeAnonymousFlatArray(), expectedValue, 'Correctly created an anonymous flat array showing fully destroyed ship parts');
     });
   });
 
